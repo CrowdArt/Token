71eef2af5b223e0ce87e4fa2352ed5507a7a8bd3
Vuk Djukic <vuchina@icloud.com>
2019-05-13 13:56:29 -0700

    Update Readme.bash

--- SFDX/Readme.bash
+++ SFDX/Readme.bash
@@ -1,10 +1,10 @@
-* `sfdx force:mdapi:deploy -c --help`
-* `sfdx force:mdapi:deploy:report vuk.djukic_external.k2@coinbase.com.onb --help`
-* `vukdjukic::~/Documents/sfdx/staging-> `
-* `ld`
-* `sfdx force:org:list`
-* `sfdx force:source:retrieve -u staging -x ./package.xml`
-* `sfdx force:org:list --all`
+* sfdx force:mdapi:deploy -c --help
+* sfdx force:mdapi:deploy:report vuk.djukic_external.k2@coinbase.com.onb --help
+* vukdjukic::~/Documents/sfdx/staging->
+* ld
+* sfdx force:org:list
+* sfdx force:source:retrieve -u staging -x ./package.xml
+* sfdx force:org:list --all
 sfdx force:alias:set --help
 sfdx force:source:retrieve -u partial -x ./package.xml
 
