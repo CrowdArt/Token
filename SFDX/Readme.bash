6560ac2ce06053b43872556dfa7b57884089957f
Vuk Djukic <vuchina@icloud.com>
2019-05-13 13:53:42 -0700

    Update Readme.bash

--- SFDX/Readme.bash
+++ SFDX/Readme.bash
@@ -6,5 +6,5 @@
 * `sfdx force:source:retrieve -u staging -x ./package.xml`
 * `sfdx force:org:list --all`
 * `sfdx force:alias:set --help`
-* `sfdx force:source:retrieve -u partial -x ./package.xml`
-* 
+sfdx force:source:retrieve -u partial -x ./package.xml
+
